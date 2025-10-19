a922d34ed75afc8095c7630688c6b7a36f7f9823
amiezeng <148655225+amiezeng@users.noreply.github.com>
2025-10-18 21:39:34 -0400

    Delete VSCode directory

--- VSCode/Test.java/testing1.java
+++
@@ -1,9 +0,0 @@
-public class testing1 {
-    
-    
-    public static void main(String[] args) {
-        System.out.println("Hello, VS Code!"); // Prints "Hello, VS Code!" and a new line
-        System.out.print("This is a test."); // Prints "This is a test." on the same line
-        System.out.println(" Another line."); // Prints " Another line." on the same line, then a new line
-    }
-}
